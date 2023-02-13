49c6245fb704e78b72a7612b724fd7b6dd0da815
Al Hasanul Banna <tutul5800@gmail.com>
2023-02-13 21:27:29 +0600

    add key event

--- js/index.js
+++ js/index.js
@@ -1,4 +1,14 @@
 document.getElementById("login-btn").addEventListener("click", function () {
+  verifyLogin();
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    verifyLogin();
+  }
+});
+
+function verifyLogin() {
   const userEmail = document.getElementById("user-email");
   const email = userEmail.value;
 
@@ -11,4 +21,20 @@ document.getElementById("login-btn").addEventListener("click", function () {
   } else {
     alert("Invalid Username or Password");
   }
-});
+}
+
+//ONLY FOR MOUSE CLICK
+// document.getElementById("login-btn").addEventListener("click", function () {
+//   const userEmail = document.getElementById("user-email");
+//   const email = userEmail.value;
+
+//   const userPass = document.getElementById("user-pass");
+//   const password = userPass.value;
+
+//   // verify email and password
+//   if (email === "ahbanna@gmail.com" && password === "123456") {
+//     window.location.href = "bank.html";
+//   } else {
+//     alert("Invalid Username or Password");
+//   }
+// });
