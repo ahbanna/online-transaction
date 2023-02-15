f72dcd114636e69847d075c9b1264b24c404e785
Al Hasanul Banna <tutul5800@gmail.com>
2023-02-16 02:34:12 +0600

    Used common function

--- js/Withdraw.js
+++ js/Withdraw.js
@@ -1,33 +1,51 @@
-// ---------- NOT USING COMMON FUNCTION -------------//
+// // ---------- USING COMMON FUNCTION -------------//
 document.getElementById("withdraw-btn").addEventListener("click", function () {
-  // New withdraw
-  const getNewWithdraw = document.getElementById("withdraw-input");
-  const newWithdraw = parseFloat(getNewWithdraw.value);
+  const newWithdraw = getInputFieldById("withdraw-input");
+  const preWithdraw = getElementValueById("pre-withdraw");
+  const preBalance = getElementValueById("pre-balance");
 
-  //clear
-  getNewWithdraw.value = "";
-
-  // previous withdraw
-  const getPreWithdraw = document.getElementById("pre-withdraw");
-  const preWithdraw = parseFloat(getPreWithdraw.innerText);
-
-  // get previous balance
-  const getPreBalance = document.getElementById("pre-balance");
-  const preBalance = parseFloat(getPreBalance.innerText);
-
-  // if the desire withdraw amount is grater than balance
   if (newWithdraw > preBalance) {
     alert("Insufficient Balance");
     return;
   }
 
-  // total withdraw
   const totalWithdraw = preWithdraw + newWithdraw;
-  // set total withdraw
-  getPreWithdraw.innerText = totalWithdraw;
+  setvalue("pre-withdraw", totalWithdraw);
 
-  // calculate total balance
   const totalBalance = preBalance - newWithdraw;
-  // show total balance
-  getPreBalance.innerText = totalBalance;
+  setvalue("pre-balance", totalBalance);
 });
+
+// // ---------- NOT USING COMMON FUNCTION -------------//
+// document.getElementById("withdraw-btn").addEventListener("click", function () {
+//   // New withdraw
+//   const getNewWithdraw = document.getElementById("withdraw-input");
+//   const newWithdraw = parseFloat(getNewWithdraw.value);
+
+//   //clear
+//   getNewWithdraw.value = "";
+
+//   // previous withdraw
+//   const getPreWithdraw = document.getElementById("pre-withdraw");
+//   const preWithdraw = parseFloat(getPreWithdraw.innerText);
+
+//   // get previous balance
+//   const getPreBalance = document.getElementById("pre-balance");
+//   const preBalance = parseFloat(getPreBalance.innerText);
+
+//   // if the desire withdraw amount is grater than balance
+//   if (newWithdraw > preBalance) {
+//     alert("Insufficient Balance");
+//     return;
+//   }
+
+//   // total withdraw
+//   const totalWithdraw = preWithdraw + newWithdraw;
+//   // set total withdraw
+//   getPreWithdraw.innerText = totalWithdraw;
+
+//   // calculate total balance
+//   const totalBalance = preBalance - newWithdraw;
+//   // show total balance
+//   getPreBalance.innerText = totalBalance;
+// });
--- js/deposit.js
+++ js/deposit.js
@@ -1,27 +1,67 @@
-// ---------- NOT USING COMMON FUNCTION -------------//
+// // ---------- USING COMMON FUNCTION -------------//
+// input field function
+function getInputFieldById(id) {
+  const inputField = document.getElementById(id);
+  const inputFieldValue = parseFloat(inputField.value);
+  inputField.value = "";
+  return inputFieldValue;
+}
+
+// element function
+function getElementValueById(id) {
+  const element = document.getElementById(id);
+  const elementValue = parseFloat(element.innerText);
+  return elementValue;
+}
+
+// set value function
+function setvalue(id, value) {
+  const getElementToSetValue = document.getElementById(id);
+  getElementToSetValue.innerText = value;
+}
+
 document.getElementById("deposit-btn").addEventListener("click", function () {
   // new deposit
-  const getNewDeposit = document.getElementById("deposit-input");
-  const newDeposit = parseFloat(getNewDeposit.value);
-
-  // previous deposit
-  const getPreDeposit = document.getElementById("pre-deposit");
-  const preDeposit = parseFloat(getPreDeposit.innerText);
-
+  const newDeposit = getInputFieldById("deposit-input");
+  // pre deposit
+  const preDeposit = getElementValueById("pre-deposit");
   // total deposit
-  const totalDeposit = preDeposit + newDeposit;
+  const totalDeposit = newDeposit + preDeposit;
+
   // set total deposit
-  getPreDeposit.innerText = totalDeposit;
-
-  // previous balance
-  const getPreBalance = document.getElementById("pre-balance");
-  const preBalance = parseFloat(getPreBalance.innerText);
-
+  setvalue("pre-deposit", totalDeposit);
+  // pre balance
+  const preBalance = getElementValueById("pre-balance");
   // total balance
   const totalBalance = preBalance + newDeposit;
-  // set total balance
-  getPreBalance.innerText = totalBalance;
-
-  // clear
-  getNewDeposit.value = "";
+  //set total balance
+  setvalue("pre-balance", totalBalance);
 });
+
+// // ---------- NOT USING COMMON FUNCTION -------------//
+// document.getElementById("deposit-btn").addEventListener("click", function () {
+//   // new deposit
+//   const getNewDeposit = document.getElementById("deposit-input");
+//   const newDeposit = parseFloat(getNewDeposit.value);
+
+//   // previous deposit
+//   const getPreDeposit = document.getElementById("pre-deposit");
+//   const preDeposit = parseFloat(getPreDeposit.innerText);
+
+//   // total deposit
+//   const totalDeposit = preDeposit + newDeposit;
+//   // set total deposit
+//   getPreDeposit.innerText = totalDeposit;
+
+//   // previous balance
+//   const getPreBalance = document.getElementById("pre-balance");
+//   const preBalance = parseFloat(getPreBalance.innerText);
+
+//   // total balance
+//   const totalBalance = preBalance + newDeposit;
+//   // set total balance
+//   getPreBalance.innerText = totalBalance;
+
+//   // clear
+//   getNewDeposit.value = "";
+// });
